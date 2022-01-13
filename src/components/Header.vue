d5268f8a9fe0a98c5ba9a63c69d0045b9dbbc124
Alessio2021 <alessiobut1911@gmail.com>
2022-01-13 15:07:35 +0100

    header a

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,23 @@
 <template>
-  <h1>Ciao Ciro</h1>
+  <header class="container">
+      <div class="header-sx">
+          <img src="../assets/img/dc-logo.png" alt="logo-dc">
+      </div>
+      <div class="header-dx">
+          <ul>
+              <li> <a href="">characters</a></li>
+              <li> <a href="">comics</a></li>
+              <li> <a href="">movies</a></li>
+              <li> <a href="">tv</a></li>
+              <li> <a href="">games</a></li>
+              <li> <a href="">collectibles</a></li>
+              <li> <a href="">videos</a></li>
+              <li> <a href="">fans</a></li>
+              <li> <a href="">news</a></li>
+              <li> <a href="">shop</a></li>
+          </ul>
+      </div>
+  </header>
 </template>
 
 <script>
@@ -8,6 +26,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    header {
+    display: flex;
+    
+    }
+    ul {
+        display: flex;
+        li {
+            list-style-type: none;
+            margin: 0 1em;
+            justify-content: end;
+            justify-items: center;
+            a {
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+        }
+    }
 </style>
